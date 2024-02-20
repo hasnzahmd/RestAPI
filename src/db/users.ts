b128d0280adfe1988f4603d4ebfa892bef8f1141
hassanahmad.6h1@gmail.com <hassanahmad.6h1@gmail.com>
2024-02-20 23:45:35 +0500

    update

--- src/db/users.ts
+++ src/db/users.ts
@@ -13,12 +13,15 @@ const UserSchema = new mongoose.Schema({
 export const UserModel = mongoose.model('Users', UserSchema);
 
 export const getUsers = () => UserModel.find();
+
 export const getUserByEmail = (email: string) => UserModel.findOne({email});
-export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({
-    'authentication.sessionToken': sessionToken,
-});
+
+export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({'authentication.sessionToken': sessionToken,});
+
 export const getUserByID = (id: string) => UserModel.findById(id);
-export const createUser = (values: Record<string, any>) => new UserModel(values)
-    .save().then((user) => user.toObject());
+
+export const createUser = (values: Record<string, any>) => new UserModel(values).save().then((user) => user.toObject());
+
 export const deleteUserById = (id: string) => UserModel.findByIdAndDelete({_id: id});
+
 export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
